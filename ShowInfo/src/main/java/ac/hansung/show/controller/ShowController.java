dfcf6b884e2987bc23d8cf1f2a7220aa9891c3f3
MJ <MJ@MJ-PC>
2013-11-01 17:41:04 +0900

    test

--- ShowInfo/src/main/java/ac/hansung/show/controller/ShowController.java
+++ ShowInfo/src/main/java/ac/hansung/show/controller/ShowController.java
@@ -17,6 +17,7 @@ import ac.hansung.show.service.PerforListVO;
 import ac.hansung.show.service.PlaceVO;
 import ac.hansung.show.service.ShowService;
 
+// 주석
 @Controller
 public class ShowController {
 	@Resource
